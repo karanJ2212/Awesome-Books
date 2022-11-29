8be0d19c4ea71ee45faa640b7668c15c69d4d267
PrantoshB <pbiswas1007@gmail.com>
2022-11-29 14:13:24 +0530

    Style: Add styles to the HTML file and Fix Linters Errors

--- script.js
+++ script.js
@@ -7,26 +7,6 @@ const addButton = document.querySelector(".addbook");
 
 let Books = [];
 
-// function BookData() {
-
-// }
-
-// function remove(index) {
-
-// }
-
-// addButton.addEventListener("click", () => {
-//   const book = {
-//     Title: Title.value,
-//     author: author.value,
-//   };
-//   Books.push(book);
-//   BookData();
-//   localStorage.setItem("Books", JSON.stringify(Books));
-// });
-
-/////////////////////////////////////////////////////////////////////////////////////////
-// book class
 
 class Book {
   constructor(title, author) {
@@ -42,12 +22,11 @@ class UI {
     registeredBooks.innerHTML = "";
     for (let i = 0; i < Books.length; i++) {
       registeredBooks.innerHTML += `
-      <div>
-        <p class="Title">${Books[i].Title}</p>
-        <p class="author">${Books[i].author}</p>
+      <div class="title-and-author"> 
+        <p class="Title">"${Books[i].title}" by  ${Books[i].author}</p>
         <button class="button" onclick="remove(${i})">remove</button>
-        <hr/>
       </div>
+
      `;
       Title.value = "";
       author.value = "";
@@ -62,23 +41,18 @@ window.onload = () => {
   }
   UI.addBooks();
 };
-//store class
-
-// dom events
 
 //add
+
 addButton.addEventListener("click", () => {
   const book = new Book(Title.value, author.value);
-  // const book = {
-  //   Title: Title.value,
-  //   author: author.value,
-  // };
   Books.push(book);
   UI.addBooks();
   localStorage.setItem("Books", JSON.stringify(Books));
 });
 
 //remove
+
 function remove(index) {
   Books.splice(index, 1);
   UI.addBooks();
