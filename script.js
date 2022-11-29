f5995b20d758860e23fe495b3fa90da233816fad
karan jain <karanjain2212@gmail.com>
2022-11-29 13:40:41 +0530

    Add classes

--- script.js
+++ script.js
@@ -1,47 +1,86 @@
 /* eslint-disable no-unused-vars */
 
-const Title = document.querySelector('.title');
-const author = document.querySelector('.author');
-const registeredBooks = document.querySelector('.book-list');
-const addButton = document.querySelector('.addbook');
+const Title = document.querySelector(".title");
+const author = document.querySelector(".author");
+const registeredBooks = document.querySelector(".book-list");
+const addButton = document.querySelector(".addbook");
 
 let Books = [];
 
-function BookData() {
-  registeredBooks.innerHTML = '';
-  for (let i = 0; i < Books.length; i++) {
-    registeredBooks.innerHTML += `
-    <div>
-      <p class="Title">${Books[i].Title}</p>
-      <p class="author">${Books[i].author}</p>
-      <button class="button" onclick="remove(${i})">remove</button>
-      <hr/>
-    </div>
-   `;
-    Title.value = '';
-    author.value = '';
+// function BookData() {
+
+// }
+
+// function remove(index) {
+
+// }
+
+// addButton.addEventListener("click", () => {
+//   const book = {
+//     Title: Title.value,
+//     author: author.value,
+//   };
+//   Books.push(book);
+//   BookData();
+//   localStorage.setItem("Books", JSON.stringify(Books));
+// });
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// book class
+
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
 }
 
-function remove(index) {
-  Books.splice(index, 1);
-  BookData();
-  localStorage.setItem('Books', JSON.stringify(Books));
+//UI class
+
+class UI {
+  static addBooks() {
+    registeredBooks.innerHTML = "";
+    for (let i = 0; i < Books.length; i++) {
+      registeredBooks.innerHTML += `
+      <div>
+        <p class="Title">${Books[i].Title}</p>
+        <p class="author">${Books[i].author}</p>
+        <button class="button" onclick="remove(${i})">remove</button>
+        <hr/>
+      </div>
+     `;
+      Title.value = "";
+      author.value = "";
+      Title.focus();
+    }
+  }
 }
 
 window.onload = () => {
-  if (localStorage.getItem('Books')) {
-    Books = JSON.parse(localStorage.getItem('Books'));
+  if (localStorage.getItem("Books")) {
+    Books = JSON.parse(localStorage.getItem("Books"));
   }
-  BookData();
+  UI.addBooks();
 };
+//store class
 
-addButton.addEventListener('click', () => {
-  const book = {
-    Title: Title.value,
-    author: author.value,
-  };
+// dom events
+
+//add
+addButton.addEventListener("click", () => {
+  const book = new Book(Title.value, author.value);
+  // const book = {
+  //   Title: Title.value,
+  //   author: author.value,
+  // };
   Books.push(book);
-  BookData();
-  localStorage.setItem('Books', JSON.stringify(Books));
+  UI.addBooks();
+  localStorage.setItem("Books", JSON.stringify(Books));
 });
+
+//remove
+function remove(index) {
+  Books.splice(index, 1);
+  UI.addBooks();
+  localStorage.setItem("Books", JSON.stringify(Books));
+}
